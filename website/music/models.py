11e64f4f982a576f814db5ed466f8b31bba5ae5c
Shivaprasad Mulky <shevaprasad_d@spanservices.com>
2016-11-05 01:37:58 +0530

    Completed lesson 10

--- website/music/models.py
+++ website/music/models.py
@@ -3,3 +3,17 @@ from django.db import models
 # Create your models here.
 
 # Database model - blueprint
+
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=500)
+    album_logo = models.CharField(max_length=1000)
+
+def __str__(self):
+    return self.album_title + '' + self.artist
+
+class Song(models.Model):
+    album = models.ForeignKey(Album,on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=20)
+    song_title = models.CharField(max_length=250)
